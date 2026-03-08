74f5b129b75856899f8937fe97a9ea4f3af58361
NJE-Blade <ei253u@hallgato.nje.hu>
2026-03-08 16:47:54 +0100

    Index oldal frissítése

--- index.php
+++ index.php
@@ -1,30 +1,37 @@
 <?php
-	
-	require_once("includes/config.php");
-	require_once("includes/controller.php");
-	require_once("includes/model.php");
+	session_start();
 
+	require_once("includes/config.php");
+	require_once("includes/model.php");
+	require_once("includes/controller.php");
+	
 	$app = new Controller();
-	$db = new Model();
-	$currentPage = $app->getUrlParts()[0];
-	$publicMenuItems = $db->getPublicMenu();
+    $db = new Model();
+
+    $urlParts = $app->getUrlParts();
+    $currentPage = $urlParts[0];
+    
+	$app->runAction($currentPage);
+
+    $siteTitle = $app->getSiteTitle();
+    
+    $publicMenuItems = $db->getPublicMenu();
+
+	$templatePath = $app->getTemplatePath();
 
 ?>
 <!DOCTYPE html>
 <html lang="hu">
 <head>
-    <!-- Az oldal jelenlegi "valos" url-je:
-    https://cheerful-paprenjak-1285e6.netlify.app/ 
-    -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vaszilij-EDC</title>
+    <title><?php echo $siteTitle; ?></title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;600;700;900&family=Rajdhani:wght@300;400;500;600;700&family=Crimson+Text:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/main.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/main.css">
 </head>
 <body class="d-flex flex-column min-vh-100 <?php if($currentPage == "secret") {echo "body_secret"; } ?>" data-bs-theme="dark">
 	<?php include("pages/nav.php"); ?>
@@ -32,23 +39,12 @@
 	<main class="flex-fill">
     <div class="container">
 		<?php 
-			
-			switch ($currentPage) {
-				case "cikkek":
-					include("pages/articles.php");
-					break;
-				case "kapcsolat":
-					include("pages/connect.php");
-					break;
-				case "secret":
-					include("pages/secret.php");
-					break;
-				default:
-					include("pages/main.php");
-					break;
-			}
-		
-		?>
+                if (file_exists($templatePath)) {
+                    include($templatePath);
+                } else {
+                    include("pages/main.php");
+                }
+            ?>
 	</div>
 	</main>
 	
