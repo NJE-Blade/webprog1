e7a88bcf33ea661eafa3b1369e5ce82cb54398fa
NJE-Blade <ei253u@hallgato.nje.hu>
2026-03-11 19:15:55 +0100

    Index fájl frissítés

--- index.php
+++ index.php
@@ -39,13 +39,14 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/main.css">
 </head>
-<body class="d-flex flex-column min-vh-100 <?php if($currentPage == "secret") {echo "body_secret"; } ?>" data-bs-theme="dark">
+<body class="d-flex flex-column min-vh-100 bg-black <?php if($currentPage == "secret") {echo "body_secret"; } ?>" data-bs-theme="dark">
 	<?php include("pages/nav.php"); ?>
 
 	<main class="flex-fill">
-    <div class="container">
+    
 		<?php 
                 if (file_exists($templatePath)) {
                     include($templatePath);
@@ -53,14 +54,20 @@
                     include("pages/main.php");
                 }
             ?>
-	</div>
+	
 	</main>
 	
 	<?php include("pages/footer.php"); ?>
 	
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <script>
+        const AppConfig = {
+            baseUrl: '<?php echo BASE_URL; ?>',
+            csrfToken: '<?php echo $_SESSION['csrf_token']; ?>'
+        };
+    </script>
 	<script src="<?php echo BASE_URL; ?>assets/script.js"></script>
-	
 </body>
 </html>
