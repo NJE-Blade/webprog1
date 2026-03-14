6b86e114fe34962fe5996486398ae1301d532e35
EI253U <ei253u@hallgato.nje.hu>
2026-03-14 22:55:23 +0100

    Secret oldal frissítés

--- pages/secret.php
+++ pages/secret.php
@@ -1,3 +1,4 @@
+    <div id="secret-contain">
     <div id="contain" class="mt-5">
         <div class="team-box text-center">
             <h1 class="team-title">Team Blade</h1>
@@ -8,3 +9,4 @@
             <h2 class="member-name">Lovas Bálint</h2>
         </div>
     </div>
+</div>
