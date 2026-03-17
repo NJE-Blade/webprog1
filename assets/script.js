0bef180bf395822e1bc7646d874969dd530e6fff
EI253U <ei253u@hallgato.nje.hu>
2026-03-17 21:32:12 +0100

    assets frissítés

--- assets/script.js
+++ assets/script.js
@@ -21,43 +21,36 @@ $(document).ready(function() {
         }
     });
 
-    // Képfeltöltés AJAX-szal Summernote-on keresztül
     function uploadImage(file) {
-    let data = new FormData();
-    data.append("image", file);
+        let data = new FormData();
+        data.append("image", file);
 
-    // 1. A tokent a rejtett inputból szedjük ki
-    const tokenInput = document.querySelector('input[name="csrf_token"]');
-    if (tokenInput) {
-        data.append("csrf_token", tokenInput.value);
-    }
-
-    // 2. A BASE_URL-t az űrlap 'action' attribútumából is kinyerheted alapnak,
-    // vagy használhatod az ablak aktuális URL-jét.
-    // Ha az 1. módszert választod, akkor itt: AppConfig.baseUrl + "admin/kep-feltoltes"
-    
-    const uploadUrl = AppConfig.baseUrl + "kep-feltoltes";
-
-    fetch(uploadUrl, {
-        method: "POST",
-        headers: {
-            "X-Requested-With": "XMLHttpRequest"
-        },
-        body: data
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.url) {
-            $('#summernote').summernote('insertImage', data.url);
-        } else {
-            // Itt fog megjelenni a PHP-tól kapott hibaüzenet
-            alert("Szerver hiba: " + data.error);
+        const tokenInput = document.querySelector('input[name="csrf_token"]');
+        if (tokenInput) {
+            data.append("csrf_token", tokenInput.value);
         }
-    })
-    .catch(error => {
-        console.error("Hiba:", error);
-    });
-}
+
+        const uploadUrl = AppConfig.baseUrl + "kep-feltoltes";
+
+        fetch(uploadUrl, {
+            method: "POST",
+            headers: {
+                "X-Requested-With": "XMLHttpRequest"
+            },
+            body: data
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.url) {
+                $('#summernote').summernote('insertImage', data.url);
+            } else {
+                alert("Szerver hiba: " + data.error);
+            }
+        })
+        .catch(error => {
+            console.error("Hiba:", error);
+        });
+    }
 
 
 });
